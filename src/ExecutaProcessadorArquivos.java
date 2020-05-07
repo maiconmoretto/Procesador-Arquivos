5f2ac75673add01b65620079e3f67f028b477a73
maiconmoretto <maiconmorettos@gmail.com>
2020-05-07 11:08:45 -0300

    Finalizado metodo de teste listaArquivos

--- src/ExecutaProcessadorArquivos.java
+++ src/ExecutaProcessadorArquivos.java
@@ -4,8 +4,7 @@ public class ExecutaProcessadorArquivos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-	
+			
 
 	}
 
--- src/ProcessaArquivos.java
+++ src/ProcessaArquivos.java
@@ -2,39 +2,38 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.io.File;
+import java.io.IOException;
+
 
 public class ProcessaArquivos {
 	
-	 /**Método para lsitar os arquivos de um diretório pelo nome.
-	  * @author Maicon Moretto
-	 * @param nome String -Nome do diretório a ser lido.
+	 /**Método para listar os arquivos de um diretório pelo nome.
+	 * @author Maicon Moretto
+	 * @param caminhoArquivo String -Nome do caminho do arquivo a ser lido.
 	 * @return String - Arquivos do diretório
 	 */
-	public String listaArquivos (String nome) throws IOException {
-		
-		String path = "/home/maicon/eclipse-workspace/Procesador de Arquivos/data";
-		try (Stream<Path> paths = Files.walk(Paths.get(path))) {
-		    paths
-		        .filter(Files::isRegularFile)
-		        .forEach(System.out::println);
-		} 
-		
-		
-		return "001ç1234567891234çPedroç50000\n" + 
-		"001ç3245678865434çPauloç40000.99\n" + 
-		"002ç2345675434544345çJose da SilvaçRural\n" + 
-		"002ç2345675433444345çEduardo PereiraçRural\n" + 
-		"003ç10ç[1-10-100,2-30-2.50,3-40-3.10]çPedro\n" + 
-		"003ç08ç[1-34-10,2-33-1.50,3-40-0.10]çPaulo";
+	public String listaArquivos (String caminhoArquivo) {
+		String conteudoDiretorio = "";
+
+		File file = new File(caminhoArquivo);
+		File afile[] = file.listFiles();
+		System.out.println(afile);
+		int i = 0;
+		for (int j = afile.length; i < j; i++) {
+			File arquivos = afile[i];
+			System.out.println(arquivos.getName());
+			conteudoDiretorio+= arquivos.getName();
+		}
+				
+		return conteudoDiretorio;
 	}
 	
 
 	 /**Método para ler o conteúdo do arquivo pelo nome.
-	  * @author Maicon Moretto
+	 * @author Maicon Moretto
 	 * @param nome String -Nome do arquivo a ser lido.
+	 * @param caminhoArquivo String -Nome do caminho do arquivo a ser lido.
 	 * @return String - Conteúdo do arquivo
 	 */
 	public String leArquivoPorNome(String caminhoArquivo, String nomeArquivo) {
@@ -55,14 +54,6 @@ public class ProcessaArquivos {
 		          e.getMessage());
 		    }
 		  return conteudoArquivo;
-		/*
-		return "001ç1234567891234çPedroç50000\n" + 
-		"001ç3245678865434çPauloç40000.99\n" + 
-		"002ç2345675434544345çJose da SilvaçRural\n" + 
-		"002ç2345675433444345çEduardo PereiraçRural\n" + 
-		"003ç10ç[1-10-100,2-30-2.50,3-40-3.10]çPedro\n" + 
-		"003ç08ç[1-34-10,2-33-1.50,3-40-0.10]çPaulo";
-		*/
 	}
 	
 
