e73d9b631bd54e8aec900fd2959db38abb69a330
maiconmoretto <maiconmorettos@gmail.com>
2020-05-10 11:05:29 -0300

    Finalizado classe ProcessaPiorVendedor e refatorado arquivo de teste(adicionado vendedor pra teste)

--- src/ProcessaArquivosTest.java
+++ src/ProcessaArquivosTest.java
@@ -24,7 +24,8 @@ class ProcessaArquivosTest {
 		String conteudoArquivo = "001ç1234567891234çPedroç50000\n" + "001ç3245678865434çPauloç40000.99\n"
 				+ "002ç2345675434544345çJose da SilvaçRural\n" + "002ç2345675433444345çEduardo PereiraçRural\n"
 				+ "003ç10ç[1-10-100,2-30-2.50,3-40-3.10]çPedro\n" + "003ç08ç[1-34-10,2-33-1.50,3-40-0.10]çPaulo\n"
-				+ "002ç2345675434544345çJose da SilvaçRural\n" + "001ç1234567891234çPedroç50000";
+				+ "002ç2345675434544345çJose da SilvaçRural\n" + "001ç1234567891234çPedroç50000\n"
+				+ "003ç99ç[1-1-1]çZe";
 		gravarArq.printf(conteudoArquivo);
 		file.close();
 	}
--- src/ProcessaPiorVendedor.java
+++ src/ProcessaPiorVendedor.java
@@ -4,36 +4,32 @@ public class ProcessaPiorVendedor implements Processa {
 	@Override
 	public String processa(String conteudoArquivo) {
 		String[] linhas = conteudoArquivo.split("\n");
-		String piorVendedor = "";
+		String nomeUltimoVendedor = "";
+		double totalUltimoVendedor = 0;
+		String nomePiorVendedor = "";
 		String tipoVenda = "003";
-		for (int j = 0; j <= linhas.length; j++) {
-			String[] ultimoVendedor = { "Paulo", "2" };
+		for (int j = 0; j < linhas.length; j++) {
 			String identificadorTipoDado = linhas[j].substring(0, 3);
-			if (identificadorTipoDado.contains(tipoVenda)) {
-
+			if (identificadorTipoDado.equals(tipoVenda)) {
 				String[] colunas = linhas[j].split("ç");
 				String dadosVenda = colunas[2];
-				String nomeVendedor = colunas[3];
-				int totalVendedor = 0;
-
+				String nomeVendedorAtual = colunas[3];
+				double totalVendedorAtual = 0;
 				String[] listagemItensNota = dadosVenda.split(",");
 				for (int l = 0; l < listagemItensNota.length; l++) {
-					String idItem = listagemItensNota[l].split("-")[0].replace("[", "");
-					String quantidade = listagemItensNota[l].split("-")[1];
-					String preco = listagemItensNota[l].split("-")[2].replace("]", "");
-					System.out.println("iditem " + idItem);
-
-					System.out.println("quantidade " + quantidade);
-
-					System.out.println("preco " + preco);
-//					totalVendedor = totalVendedor + ((int)quantidade * (int)preco);
-					totalVendedor = 1;
-					piorVendedor = nomeVendedor;
+					double quantidade = Double.valueOf(listagemItensNota[l].split("-")[1]);
+					double preco = Double.valueOf(listagemItensNota[l].split("-")[2].replace("]", ""));
+					totalVendedorAtual = totalVendedorAtual + (quantidade * preco);
 				}
+				if (totalUltimoVendedor < totalVendedorAtual) {
+					nomePiorVendedor = "pior vendedor:  " + nomeUltimoVendedor;
+				} else {
+					nomePiorVendedor = "pior vendedor: " + nomeVendedorAtual ;
+				}			
+				nomeUltimoVendedor = nomeVendedorAtual;
+				totalUltimoVendedor = totalVendedorAtual;
 			}
 		}
-		System.out.println("pior" + piorVendedor);
-		return piorVendedor;
+		return nomePiorVendedor;
 	}
-
 }
